6539af91292f2a02454c234a409247972557845b
thelopster <nitipon.apaisri@gmail.com>
2020-08-29 15:03:02 +0200

    Stage 1 is clear! - added form - added function for store data and display in local storage

--- localStorage/js/main.js
+++ localStorage/js/main.js
@@ -1,1 +1,18 @@
-//Reset
+let arrName = [];
+
+function sendData() {
+  let inName = document.getElementById("nameIn").value;
+  arrName.push(inName);
+  console.log(arrName);
+  localStorage.setItem("name", JSON.stringify(arrName));
+  console.log(localStorage);
+}
+let dataName = JSON.parse(localStorage.getItem("name"));
+document.getElementById("display-info-name").innerHTML = dataName;
+
+function dropValue() {
+  let localKeys = ["name"];
+  if (localStorage !== "undefined" || 0) {
+    localKeys.forEach((k) => localStorage.removeItem(k));
+  }
+}
